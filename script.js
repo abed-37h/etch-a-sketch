f87cd81c3556495416c7f872eb4012e046255bd0
abed-37h <79279228+abed-37h@users.noreply.github.com>
2024-02-13 03:00:40 +0200

    Add a button to edit the squares number of grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const grid = document.querySelector('#grid');
 const root = document.querySelector(':root');
+const changePixelSizeBtn = document.querySelector('#change-pixel-size');
 
 const createGrid = function (n) {
     root.style.setProperty('--number-of-grid-squares', n);
@@ -13,14 +14,36 @@ const createGrid = function (n) {
     
         grid.appendChild(gridSquare);
     }
-}
+};
+
+const removeGrid = function () {
+    for (const gridSquare of Array.from(grid.childNodes)) {
+        gridSquare.remove();
+    }
+};
 
 const colorize = function (event) {
     const target = event.target;
 
     target.classList.add('colored');
-}
+};
 
 grid.addEventListener('mouseover', colorize);
+
+changePixelSizeBtn.addEventListener('click', () => {
+    let n = prompt('Enter new pixel size:\nMust be <= 100', 16);
+    if (isNaN(n)) {
+        n = 16;
+    }
+    else if (n > 100) {
+        n = 100;
+    }
+    else if (n < 8) {
+        n = 8;
+    }
+
+    removeGrid();
+    createGrid(n);
+});
 
 createGrid(16);
